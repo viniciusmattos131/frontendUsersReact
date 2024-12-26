030b6c48c3a34628cea6195e769df2d8a2f03395
vinicius.mattos.memory <vinicius.mattos@memory.com.br>
2024-12-26 17:32:04 -0300

    Finishing the users creation

--- devclub-create-users/src/pages/Home/index.jsx
+++ devclub-create-users/src/pages/Home/index.jsx
@@ -1,12 +1,67 @@
-import './style.css'
+import { useEffect, useState, useRef } from "react";
+import "./style.css";
+import Trash from "../../assets/trash.svg";
+import api from "../../services/api";
 
 function Home() {
 
-  return (
+  const [users, setUsers] = useState([]);
 
-    
-      <div>
-        <h1>Olá react</h1>
+  const inputName = useRef();
+  const inputAge = useRef();
+  const inputEmail = useRef();
+
+  async function getUsers(){
+    const usersFromApi = await api.get('/users');
+
+    setUsers(usersFromApi.data);
+  }
+
+  async function createUsers(){
+    await api.post('/users', {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value
+    });
+
+    getUsers();
+  }
+
+  async function deleteUsers(id){
+    await api.delete(`/users/${id}`);
+
+    getUsers();
+
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, [])  
+
+  return (    
+      <div className="container">
+        <form>
+          <h1>Create Users</h1>
+          <input placeholder="Name" name="name" type="text" ref={inputName}/>
+          <input placeholder="Age" name="age" type="number" ref={inputAge}/>
+          <input placeholder="Email" name="email" type="email" ref={inputEmail}/>
+          <button type="button" onClick={createUsers}>Create</button>
+        </form>
+
+        { users.map( user => (
+          <div key={user.id} className="card">
+            <div>
+              <p>Name: <span>{user.name}</span></p>
+              <p>Age: <span>{user.age}</span></p>
+              <p>Email: <span>{user.email}</span></p>
+            </div>
+            <button onClick={() => deleteUsers(user.id)}>
+              <img src={Trash}/>
+            </button>
+        </div>
+        ))}
+
+
       </div>
   )
 }
